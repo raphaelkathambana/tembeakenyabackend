9bf0c61699c12916967116cbbf61ab0a6c0860e3
Raphael Kathambana <maya12raph@gmail.com>
2024-06-17 12:40:23 +0300

    chore: Update home.blade.php with email verification message and redirect button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
-it works, yeiiiiiii
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Home</title>
+</head>
+
+<body>
+    <p>Your email has been successfully verified.</p>
+    <button onclick="redirectToApp()">Open App</button>
+
+    <script>
+        function redirectToApp() {
+            window.location.href = 'https://tembeakenyabackend.fly.dev/verify-email-success';
+        }
+    </script>
+</body>
+
+</html>
