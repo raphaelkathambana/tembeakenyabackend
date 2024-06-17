c6c9731eb2e4ea12ad51374b44242d7f309e78df
Raphael Kathambana <maya12raph@gmail.com>
2024-06-17 14:33:21 +0300

    chore: Add UserSeeder to DatabaseSeeder and create super administrator user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // create a super administrator
+        \App\Models\User::factory()->create([
+            'firstname' => 'Super',
+            'lastname' => 'Admin',
+            'username' => 'superadmin',
+            'roleNo' => 3,
+            'email' => 'codeclimberske@gmail.com',
+        ]);
+    }
+}
