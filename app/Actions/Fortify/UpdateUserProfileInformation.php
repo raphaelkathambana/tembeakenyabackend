39a2b1b8fc35a09eeb026c43bc104908ff064819
Raphael Kathambana <maya12raph@gmail.com>
2024-07-02 12:20:26 +0300

    feat: Update username validation in UpdateUserProfileInformation action

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,7 +20,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'firstName' => ['required', 'string', 'max:255'],
             'lastName' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', Rule::unique('users')->ignore($user->id)],
 
             'email' => [
                 'required',
