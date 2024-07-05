b2a5d68193d2fe0db1a402dfd9a7e3c591b4efa9
Raphael Kathambana <maya12raph@gmail.com>
2024-07-06 02:53:30 +0300

    chore: Add UserController with CRUD operations and follow/unfollow functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        return new UserCollection($users);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::with('reviews')->findOrFail($id);
+        return new UserResource($user);
+    }
+
+    public function unfollow($id)
+    {
+        $user = auth()->user();
+        $userToUnfollow = User::findOrFail($id);
+
+        $user->following()->detach($userToUnfollow->id);
+
+        return response()->json(['message' => 'User unfollowed successfully'], 200);
+    }
+
+    public function following(Request $request)
+    {
+        $user = auth()->user();
+        $following = $user->following()->paginate(10);
+        return new UserCollection($following);
+    }
+
+    public function follow($id)
+    {
+        $user = auth()->user();
+        $userToFollow = User::findOrFail($id);
+
+        if ($user->id === $userToFollow->id) {
+            return response()->json(['message' => 'You cannot follow yourself'], 400);
+        }
+
+        $user->following()->attach($userToFollow->id);
+
+        return response()->json(['message' => 'User followed successfully'], 200);
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'username' => $this->username,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'roleNo' => $this->roleNo,
+            'image_id' => $this->image_id,
+            'no_of_hikes' => $this->no_of_hikes,
+            'total_distance_walked' => $this->total_distance_walked,
+            'no_of_steps_taken' => $this->no_of_steps_taken,
+            'followers_count' => $this->followers()->count(),
+            'following_count' => $this->following()->count(),
+            'groups' => GroupResource::collection($this->whenLoaded('groups')),
+            'guideAdminsGroups' => GroupResource::collection($this->whenLoaded('guideAdminsGroups')),
+            'hikes' => HikeResource::collection($this->whenLoaded('hikes')),
+            'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
+        ];
+    }
+}
